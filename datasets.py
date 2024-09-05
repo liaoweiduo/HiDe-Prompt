646e6baeb65d937b80607ae8bed7e010dcc7daf1
pomelo <liaowd@mail.sustech.edu.cn>
2024-09-05 15:30:45 +0800

    try checkpoints

--- datasets.py
+++ datasets.py
@@ -225,30 +225,30 @@ def get_dataset(dataset, transform_train, transform_val, args, target_transform=
 
 
 def split_single_dataset(dataset_train, dataset_val, args):
-    if 'cgqa' in args.dataset.lower():
-        # big first task
-        labels = [26, 86, 2, 55, 75, 93, 16, 73, 54, 95,
-                  53, 92, 78, 13, 7, 30, 22, 24, 33, 8,
-                  43, 62, 3, 71, 45, 48, 6, 99, 82, 76,
-                  60, 80, 90, 68, 51, 27, 18, 56, 63, 74,
-                  1, 61, 42, 41, 4, 15, 17, 40, 38, 5,
-                  91, 59, 0, 34, 28, 50, 11, 35, 23, 52,
-                  10, 31, 66, 57, 79, 85, 32, 84, 14, 89,
-                  19, 29, 49, 97, 98, 69, 20, 94, 72, 77,
-                  25, 37, 81, 46, 39, 65, 58, 12, 88, 70,
-                  87,36, 21, 83, 9, 96, 67, 64, 47, 44]
-        classes_per_task = [10 * (10 - args.num_tasks + 1), 10]
-    # elif 'cobj' in args.dataset.lower():
-    #     pass
-    else:
-        nb_classes = len(dataset_val.classes)
-        # assert nb_classes % args.num_tasks == 0
-        classes_per_task = math.ceil(nb_classes / args.num_tasks)
+    # if 'cgqa' in args.dataset.lower():
+    #     # big first task
+    #     labels = [26, 86, 2, 55, 75, 93, 16, 73, 54, 95,
+    #               53, 92, 78, 13, 7, 30, 22, 24, 33, 8,
+    #               43, 62, 3, 71, 45, 48, 6, 99, 82, 76,
+    #               60, 80, 90, 68, 51, 27, 18, 56, 63, 74,
+    #               1, 61, 42, 41, 4, 15, 17, 40, 38, 5,
+    #               91, 59, 0, 34, 28, 50, 11, 35, 23, 52,
+    #               10, 31, 66, 57, 79, 85, 32, 84, 14, 89,
+    #               19, 29, 49, 97, 98, 69, 20, 94, 72, 77,
+    #               25, 37, 81, 46, 39, 65, 58, 12, 88, 70,
+    #               87,36, 21, 83, 9, 96, 67, 64, 47, 44]
+    #     classes_per_task = [10 * (10 - args.num_tasks + 1), 10]
+    # # elif 'cobj' in args.dataset.lower():
+    # #     pass
+    # else:
+    nb_classes = len(dataset_val.classes)
+    # assert nb_classes % args.num_tasks == 0
+    classes_per_task = math.ceil(nb_classes / args.num_tasks)
 
-        labels = [i for i in range(nb_classes)]
+    labels = [i for i in range(nb_classes)]
 
-        if args.shuffle:
-            random.shuffle(labels)
+    if args.shuffle:
+        random.shuffle(labels)
 
     split_datasets = list()
     mask = list()
--- training_scripts/train_cgqa_vit.sh
+++ training_scripts/train_cgqa_vit.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
 seed=42
-#for seed in 42 40 44
-#do
-#python -m torch.distributed.launch \
-#        --nproc_per_node=1 \
-#        --use_env main.py \
-#        cgqa_hideprompt_5e \
-#        --original_model vit_base_patch16_224 \
-#        --model vit_base_patch16_224 \
-#        --batch-size 24 \
-#        --data-path ../datasets \
-#        --output_dir ./output/cgqa_sup21k_multi_centroid_mlp_2_seed$seed \
-#        --epochs 20 \
-#        --sched constant \
-#        --seed $seed \
-#        --train_inference_task_only \
-#        --num_tasks 8 \
-#        --lr 0.0005
-#done
+for seed in 42 40 44
+do
+python -m torch.distributed.launch \
+        --nproc_per_node=1 \
+        --use_env main.py \
+        cgqa_hideprompt_5e \
+        --original_model vit_base_patch16_224 \
+        --model vit_base_patch16_224 \
+        --batch-size 24 \
+        --data-path ../datasets \
+        --output_dir ./output/cgqa_sup21k_multi_centroid_mlp_2_seed$seed \
+        --epochs 20 \
+        --sched constant \
+        --seed $seed \
+        --train_inference_task_only \
+        --num_tasks 10 \
+        --n_centroids 1 \
+        --lr 0.0005
+done
 
-#for seed in 42 40 44
-#do
+for seed in 42 40 44
+do
 python -m torch.distributed.launch \
 	--nproc_per_node=1 \
 	--master_port='29501' \
@@ -41,6 +42,7 @@ python -m torch.distributed.launch \
 	--sched step \
 	--larger_prompt_lr \
 	--trained_original_model ./output/cgqa_sup21k_multi_centroid_mlp_2_seed$seed \
-  --num_tasks 8 \
+  --num_tasks 10 \
+  --n_centroids 1 \
 	--output_dir ./output/cgqa_vit_pe_seed$seed
-#done
+done
