26398a80e6a20f7a5641a0a89a7ebf9fef3bfca5
pomelo <liaowd@mail.sustech.edu.cn>
2024-09-05 15:56:00 +0800

    try checkpoints

---
+++ training_scripts/train_cobj_vit.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+seed=42
+for seed in 42 40 44
+do
+python -m torch.distributed.launch \
+        --nproc_per_node=1 \
+        --use_env main.py \
+        cobj_hideprompt_5e \
+        --original_model vit_base_patch16_224 \
+        --model vit_base_patch16_224 \
+        --batch-size 24 \
+        --data-path ../datasets \
+        --output_dir ./output/cobj_sup21k_multi_centroid1_mlp_2_seed$seed \
+        --epochs 20 \
+        --sched constant \
+        --seed $seed \
+        --train_inference_task_only \
+        --num_tasks 3 \
+        --n_centroids 1 \
+        --lr 0.0005
+done
+
+for seed in 42 40 44
+do
+python -m torch.distributed.launch \
+	--nproc_per_node=1 \
+	--master_port='29502' \
+	--use_env main.py \
+	cobj_hideprompt_5e \
+	--model vit_base_patch16_224 \
+	--original_model vit_base_patch16_224 \
+	--batch-size 24 \
+	--epochs 50 \
+	--data-path ../datasets \
+	--ca_lr 0.005 \
+	--crct_epochs 30 \
+	--seed $seed \
+	--prompt_momentum 0.01 \
+	--reg 0.1 \
+	--length 5 \
+	--sched step \
+	--larger_prompt_lr \
+	--trained_original_model ./output/cobj_sup21k_multi_centroid1_mlp_2_seed$seed \
+  --num_tasks 3 \
+  --n_centroids 1 \
+	--output_dir ./output/cobj_vit_pe_centroid1_seed$seed
+done
