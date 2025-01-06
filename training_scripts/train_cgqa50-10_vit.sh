0e893c3997aa816777a6c4248e5d6689143616d8
pomelo <liaowd@mail.sustech.edu.cn>
2025-01-06 09:49:27 +0800

    try checkpoints

--- training_scripts/train_cgqa50-10_vit.sh
+++ training_scripts/train_cgqa50-10_vit.sh
@@ -11,14 +11,16 @@ python -m torch.distributed.launch \
         --model vit_base_patch16_224 \
         --batch-size 24 \
         --data-path ../datasets \
-        --output_dir ./output/cgqa50_10_sup21k_multi_centroid1_mlp_2_seed$seed \
+        --output_dir ./output/cgqa50_10_sup21k_multi_centroid1_replay1_mlp_2_seed$seed \
         --epochs 20 \
         --sched constant \
+        --ca_lr 0.005 \
+        --crct_epochs 1 \
         --seed $seed \
         --train_inference_task_only \
         --n_centroids 1 \
         --shuffle \
-        --lr 0.0005 > ./output/cgqa50_10_sup21k_multi_centroid1_mlp_2_seed${seed}.out 2>&1
+        --lr 0.0005 > ./output/cgqa50_10_sup21k_multi_centroid1_replay1_mlp_2_seed${seed}.out 2>&1
 
 python -m torch.distributed.launch \
 	--nproc_per_node=1 \
@@ -31,15 +33,15 @@ python -m torch.distributed.launch \
 	--epochs 20 \
 	--data-path ../datasets \
 	--ca_lr 0.005 \
-	--crct_epochs 10 \
+	--crct_epochs 1 \
 	--seed $seed \
 	--prompt_momentum 0.01 \
 	--reg 0.1 \
 	--length 4 \
 	--sched step \
 	--larger_prompt_lr \
-	--trained_original_model ./output/cgqa50_10_sup21k_multi_centroid1_mlp_2_seed$seed \
+	--trained_original_model ./output/cgqa50_10_sup21k_multi_centroid1_replay1_mlp_2_seed$seed \
   --n_centroids 1 \
   --shuffle \
-	--output_dir ./output/cgqa50_10_vit_pe_centroid1_seed$seed > ./output/cgqa50_10_vit_pe_centroid1_seed${seed}.out 2>&1
+	--output_dir ./output/cgqa50_10_vit_pe_centroid1_replay1_seed$seed > ./output/cgqa50_10_vit_pe_centroid1_replay1_seed${seed}.out 2>&1
 done
